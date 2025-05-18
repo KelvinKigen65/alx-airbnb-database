ef2fe8c617f5ed987b2ebdc658dab4099ab91352
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2025-05-18 20:38:00 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -4,9 +4,8 @@ GROUP BY user_id;
 
 
 SELECT property_id, COUNT(*) AS total_bookings,
-       ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS row_num
+       RANK() OVER (ORDER BY COUNT(*) DESC) AS rank,
+       ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS row_number
 FROM bookings
 GROUP BY property_id
-ORDER BY row_num;
-
-
+ORDER BY rank;
