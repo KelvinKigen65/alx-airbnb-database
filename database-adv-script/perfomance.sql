61e9703b3ea8fdd177cf3e2df791d98797f6e385
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2025-05-18 20:45:18 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,9 +1,10 @@
 
-SELECT b.id AS booking_id, b.booking_date, b.check_in, b.check_out,
-       u.id AS user_id, u.username, u.email,
-       p.id AS property_id, p.name AS property_name, p.location,
-       pay.id AS payment_id, pay.amount, pay.status
+SELECT b.id AS booking_id, b.check_in, b.check_out,
+       u.username, p.name AS property_name, pay.amount
 FROM bookings b
 JOIN users u ON b.user_id = u.id
 JOIN properties p ON b.property_id = p.id
-JOIN payments pay ON b.payment_id = pay.id;
+JOIN payments pay ON b.payment_id = pay.id
+WHERE b.booking_date >= NOW() - INTERVAL 6 MONTH
+AND pay.status = 'Completed'
+ORDER BY b.booking_date DESC;
