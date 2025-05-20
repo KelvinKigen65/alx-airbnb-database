871667e85f07de13ba0636ca50944a1efe617c81
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2025-05-21 00:47:44 +0300

    database update

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,14 @@
 
+SELECT users.user_id, users.name, bookings.booking_id, bookings.property_id, bookings.date
+FROM users
+INNER JOIN bookings ON users.user_id = bookings.user_id;
+
+
+SELECT properties.property_id, properties.name, reviews.review_id, reviews.comment, reviews.rating
+FROM properties
+LEFT JOIN reviews ON properties.property_id = reviews.property_id;
+
+
+SELECT users.user_id, users.name, bookings.booking_id, bookings.property_id, bookings.date
+FROM users
+FULL OUTER JOIN bookings ON users.user_id = bookings.user_id;
